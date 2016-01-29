3a4ada5b3cb50ffa0aef0aede8759ccef1d7c95d
Liffes <woj.g95@gmail.com>
2016-01-29 23:59:51 +0100

    Dodany obiekt dziewczynki, podstawowe funkcje przycisków telefonu

---
+++ Assets/Scripts/GirlScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GirlScript : MonoBehaviour {
+
+	public string hairColor;
+	public string skinColor;
+	public string shirtColor;
+	public string pantsColor;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PhoneScripts.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PhoneScripts : MonoBehaviour {
+
+	private int girlNo;
+	public Text hairText;
+	public Text bodyText;
+	public Text legsText;
+	public Text skinText;
+	public GirlScript [] girls;
+
+	// Use this for initialization
+	void Start () {
+		//girls = ....   Funkcja przekazująca dziewczynki z jakiegos kontrolera misji
+		girlNo = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ShowNextGirl(){
+		girlNo++;
+		setTexts ();
+		Debug.Log ("Showing next grill");
+	}
+
+	public void ShowPreviousGrill(){
+		girlNo--;
+		setTexts ();
+		Debug.Log ("Showing prevoius grill");
+	}
+
+	public void HidePhone(){
+		Debug.Log ("Phone hidden");
+	}
+
+	private void setTexts(){ //PH
+		skinText.text = "Skin " + girlNo;
+		hairText.text = "Hair " + girlNo;
+		bodyText.text = "Shirt " + girlNo;
+		legsText.text = "Legs " + girlNo;
+	}
+}
