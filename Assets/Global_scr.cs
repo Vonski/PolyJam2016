cec80f43d2c223eb3cb00d19a2d35b18842c04d4
szymon112233 <szymmon123@gmail.com>
2016-01-30 23:24:41 +0100

    Merge głowny

---
+++ Assets/Global_scr.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Global_scr : MonoBehaviour {
+
+    public Vector3 target_position;
+
+    // Use this for initialization
+    void Start () {
+        target_position = new Vector3();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+}
---
+++ Assets/Idz.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Idz : MonoBehaviour, IPlayerState {
+
+    public float speed;
+    Vector3 offset;
+    //Random rand;
+
+    void OnEnable()
+    {
+        speed = Random.Range(2.5F, 4.5F);
+        StartCoroutine(ChangeSpeed());
+    }
+
+    void Update()
+    {
+        transform.position += offset * speed * Time.deltaTime;
+    }
+
+    IEnumerator ChangeSpeed()
+    {
+        yield return new WaitForSeconds(Random.Range(1F, 2.0F));
+        GetComponent<PlayerStateMachine>().SetState<Stoj>();
+        offset = new Vector3(Random.Range(-1.0F, 1.0F), Random.Range(-1.0F, 1.0F), 0);
+    }
+}
