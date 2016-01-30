f122eb0b47c09a3a55a3b39ebfbf83b4044f9ef1
Vonski <daniellodelapedro@interia.pl>
2016-01-30 22:07:55 +0100

    Kawałek maszyny stanów

---
+++ Assets/Patrol.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patrol : MonoBehaviour, IPlayerState {
+
+    public float speed;
+    Vector3 offset;
+    public Vector3 target_position;
+    public bool boom;
+    public GameObject tmp;
+
+    void Start()
+    {
+        boom = true;
+        tmp = GameObject.Find("Global");
+    }
+
+    void OnEnable()
+    {
+        speed = Random.Range(3F, 4F);
+        StartCoroutine(ChangeSpeed());
+        boom = true;
+    }
+
+    void Update()
+    {
+        transform.position += offset * speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            var v3 = GameObject.FindGameObjectWithTag("Player");
+            tmp.GetComponent<Global_scr>().target_position = v3.transform.position;
+
+            boom = false;
+
+            
+
+        }
+    }
+
+    IEnumerator ChangeSpeed()
+    {
+        yield return new WaitForSeconds(Random.Range(1F, 2.0F));
+        if(boom)
+            GetComponent<PlayerStateMachine>().SetState<Stoj>();
+        else
+            GetComponent<PlayerStateMachine>().SetState<Target>();
+        offset = new Vector3(Random.Range(-1.0F, 1.0F), Random.Range(-0.05F, 0.05F), 0);
+    }
+}
---
+++ Assets/PlayerStateMachine.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStateMachine : StateMachine<IPlayerState> {
+    void Awake()
+    {
+        SetState<Patrol>();
+    }
+	
+}
+
+public interface IPlayerState : IMachineState
+{
+
+}
