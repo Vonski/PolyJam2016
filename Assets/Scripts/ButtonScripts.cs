06a0c5e8594b5815d963ba441bc3b50bc100c1d6
szymon112233 <szymmon123@gmail.com>
2016-01-30 01:11:06 +0100

    Pierwsza wersja menu
    1. funkcja dla guzików , placeholderowa grafika

---
+++ Assets/Scripts/ButtonScripts.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScripts : MonoBehaviour {
+
+    public void go_to_shop();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void go_to_shop()
+    {
+        GameObject.Find("main").SetActive(false);
+        GameObject.Find("shop").SetActive(true);
+    }
+}
