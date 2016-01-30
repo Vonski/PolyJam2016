f4983f9da68218ae8c9b754b4e530b0052832cd2
szymon112233 <szymmon123@gmail.com>
2016-01-30 03:13:44 +0100

    Menu v.2
    Większość guzików podpięta pod funkcje. Trzeba dopracować go_to_level

--- Assets/Scripts/ButtonScripts.cs
+++ Assets/Scripts/ButtonScripts.cs
@@ -3,12 +3,17 @@ using System.Collections;
 
 public class ButtonScripts : MonoBehaviour {
 
-    public void go_to_shop();
+    private GameObject obj_shop, obj_main, obj_credits;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    void Start () {
+        obj_shop =  GameObject.Find("shop");
+        obj_main = GameObject.Find("main");
+        obj_credits = GameObject.Find("credits");
+        obj_shop.SetActive(false);
+        obj_credits.SetActive(false);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -16,7 +21,23 @@ public class ButtonScripts : MonoBehaviour {
 	}
     public void go_to_shop()
     {
-        GameObject.Find("main").SetActive(false);
-        GameObject.Find("shop").SetActive(true);
+        obj_main.SetActive(false);
+        obj_shop.SetActive(true);
+    }
+    public void go_to_credtis()
+    {
+        obj_main.SetActive(false);
+        obj_credits.SetActive(true);
+    }
+    public void go_to_mainmenu()
+    {
+        obj_credits.SetActive(false);
+        obj_shop.SetActive(false);
+        obj_main.SetActive(true);
+    }
+    public void go_to_level(string lvl)
+    {
+        Debug.Log("Changing level...Rly...trust me...("+lvl+")");
+        //Application.LoadLevel(lvl);
     }
 }
