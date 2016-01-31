941332775e62a693504416a9b4458a3df165a15a
lidalida <lidka.wojciechowska1996@gmail.com>
2016-01-31 13:26:33 +0100

    patrol

--- Assets/Patrol.cs
+++ Assets/Patrol.cs
@@ -46,7 +46,7 @@ public class Patrol : MonoBehaviour, IPlayerState {
             else
                 GetComponent<PlayerStateMachine>().SetState<Target>();
             offset = new Vector3(Random.Range(-1.0F, 1.0F), Random.Range(-0.05F, 0.05F), 0);
-            Debug.Log("Randozmied");
+            //Debug.Log("Randozmied");
         }
     }
 }
