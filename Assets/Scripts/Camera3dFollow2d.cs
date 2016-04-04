39063d3b3f2ad02f3239ee10d335547c237a869a
melc.mo <melc.mo@nom.ail>
2016-04-04 16:20:28 +0200

    disabled rotations for characters. Camera controlling script. [updated prefabs].

---
+++ Assets/Scripts/Camera3dFollow2d.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera3dFollow2d : MonoBehaviour {
+
+    public GameObject playerObject;
+
+    private Vector3 offset;
+    private Vector3 cameraStartingPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        //set up initial offset of the camera to player
+        offset = transform.position - playerObject.transform.position;
+        cameraStartingPosition = transform.position;
+    }
+    
+    // update camera position here after all objects already moved
+    void LateUpdate()
+    {
+        transform.position = playerObject.transform.position + offset;
+        transform.position = new Vector3(transform.position.x, cameraStartingPosition.y, cameraStartingPosition.z);
+    }
+
+}
